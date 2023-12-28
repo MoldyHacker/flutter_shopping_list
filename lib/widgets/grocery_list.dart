e4661a1ae36349eaa483f42970bb7c3533d57eba
MoldyHacker <56985918+MoldyHacker@users.noreply.github.com>
2023-12-28 11:36:34 -0600

    Refactor grocery list and new item widgets

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,11 +1,10 @@
+import 'package:flutter/material.dart';
 import 'dart:convert';
 
-import 'package:flutter/material.dart';
-
 import 'package:http/http.dart' as http;
+
 import 'package:shopping_list/data/categories.dart';
 import 'package:shopping_list/models/category.dart';
-
 import 'package:shopping_list/models/grocery_item.dart';
 import 'package:shopping_list/widgets/new_item.dart';
 
@@ -32,15 +31,14 @@ class _GroceryListState extends State<GroceryList> {
     );
     final response = await http.get(url);
 
-    final Map<String, dynamic> listData =
-        json.decode(response.body);
-    final List<GroceryItem> _loadedItems = [];
+    final Map<String, dynamic> listData = json.decode(response.body);
+    final List<GroceryItem> loadedItems = [];
     for (final item in listData.entries) {
       final category = categories.entries
           .firstWhere(
               (catItem) => catItem.value.title == item.value['category'])
           .value;
-      _loadedItems.add(
+      loadedItems.add(
         GroceryItem(
           id: item.key,
           name: item.value['name'],
@@ -50,18 +48,24 @@ class _GroceryListState extends State<GroceryList> {
       );
     }
     setState(() {
-      _groceryItems = _loadedItems;
+      _groceryItems = loadedItems;
     });
   }
 
   void _addItem() async {
-    await Navigator.of(context).push<GroceryItem>(
+    final newItem = await Navigator.of(context).push<GroceryItem>(
       MaterialPageRoute(
         builder: (ctx) => const NewItem(),
       ),
     );
 
-    _loadItems();
+    if (newItem == null) {
+      return;
+    }
+
+    setState(() {
+      _groceryItems.add(newItem);
+    });
   }
 
   void _removeItem(GroceryItem item) {
