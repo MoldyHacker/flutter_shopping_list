a77e711085324e4035b9dde89d88b84964d184e3
MoldyHacker <56985918+MoldyHacker@users.noreply.github.com>
2023-12-27 20:59:14 -0600

    Refactor form validation and input handling in NewItem widget

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -25,8 +25,15 @@ class _NewItemState extends State<NewItem> {
                 decoration: const InputDecoration(
                   labelText: 'Name',
                 ),
-                validator: (value) =>
-                    value!.isEmpty ? 'Please enter a name' : null,
+                validator: (value) {
+                  if (value == null ||
+                      value.isEmpty ||
+                      value.trim().length <= 1 ||
+                      value.trim().length > 50) {
+                    return 'Must be between 1 and 50 characters.';
+                  }
+                  return null;
+                },
               ),
               Row(
                 crossAxisAlignment: CrossAxisAlignment.end,
@@ -37,32 +44,53 @@ class _NewItemState extends State<NewItem> {
                         labelText: 'Quantity',
                       ),
                       initialValue: '1',
+                      keyboardType: TextInputType.number,
+                      validator: (value) {
+                        if (value == null ||
+                            value.isEmpty ||
+                            int.tryParse(value) == null ||
+                            int.tryParse(value)! <= 0) {
+                          return 'Must be between 1 and 50 characters.';
+                        }
+                        return null;
+                      },
                     ),
                   ),
                   const SizedBox(width: 8),
                   Expanded(
-                    child: DropdownButtonFormField(
-                      items: [
-                        for (final category in categories.entries)
-                          DropdownMenuItem(
-                            value: category.value,
-                            child: Row(
-                              children: [
-                                Container(
-                                  width: 16,
-                                  height: 16,
-                                  color: category.value.color,
-                                ),
-                                const SizedBox(
-                                  width: 6,
-                                ),
-                                Text(category.value.title),
-                              ],
-                            ),
+                    child: DropdownButtonFormField(items: [
+                      for (final category in categories.entries)
+                        DropdownMenuItem(
+                          value: category.value,
+                          child: Row(
+                            children: [
+                              Container(
+                                width: 16,
+                                height: 16,
+                                color: category.value.color,
+                              ),
+                              const SizedBox(
+                                width: 6,
+                              ),
+                              Text(category.value.title),
+                            ],
                           ),
-                      ],
-                      onChanged: (value) {}
-                    ),
+                        ),
+                    ], onChanged: (value) {}),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 12),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text('Reset'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('Add Item'),
                   ),
                 ],
               )
